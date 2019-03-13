49efeabb342ab7f4719352856356e1757197f98e
zjjfly <zjjblue@gmail.com>
2019-03-13 16:57:11 +0800

    delete main

--- container/container_process.go
+++ container/container_process.go
@@ -1,24 +1,10 @@
 package container
 
 import (
-	log "github.com/Sirupsen/logrus"
 	"os"
 	"os/exec"
-	"strings"
 	"syscall"
 )
-
-func RunContainerInitProcess(command string, args []string) error {
-	log.Infof("command %s", command)
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	cmds := strings.Split(command, " ")
-	err := syscall.Exec(cmds[0], cmds[0:], os.Environ())
-	if err != nil {
-		log.Errorf(err.Error())
-	}
-	return nil
-}
 
 func NewParentProcess(tty bool, command string) *exec.Cmd {
 	args := []string{"init", command}
--- container/init.go
+++ container/init.go
@@ -1,1 +1,20 @@
 package container
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func RunContainerInitProcess(command string, args []string) error {
+	log.Infof("command %s", command)
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	cmds := strings.Split(command, " ")
+	err := syscall.Exec(cmds[0], cmds[0:], os.Environ())
+	if err != nil {
+		log.Errorf(err.Error())
+	}
+	return nil
+}
